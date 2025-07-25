34b1d12c208429ed57acf8f70e6b6123b6e42d7e
Ryan Nenyasha Chitate <yashryan16@gmail.com>
2025-07-25 18:23:40 +0200

    change

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from db import Base, engine
-from routes import mall, store
+from routes.mall import router as mall_router
+from routes.store import router as store_router
 from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
@@ -9,11 +10,11 @@ app = FastAPI(title="Roamwise API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change to frontend domain in production
+    allow_origins=["*"],  # For dev only
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(mall)
-app.include_router(store)
+app.include_router(mall_router, prefix="/api/malls", tags=["Malls"])
+app.include_router(store_router, prefix="/api/stores", tags=["Stores"])
